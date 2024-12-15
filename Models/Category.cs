beb4221fab5cb31be4318943cc3a066e07bb941b
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 20:31:52 +0530

    category model add

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sticker_Web_dotnet.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+    }
+}
