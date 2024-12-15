b42e197a180c03b3c6158dfb293c1cf1725d7c7b
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 22:53:26 +0530

    courier repository add

---
+++ Repository/CourierRepository.cs
@@ -0,0 +1,20 @@
+﻿using Sticker_Web_dotnet.Models;
+using Sticker_Web_dotnet.Data;
+using Sticker_Web_dotnet.Repository.Interfaces;
+
+namespace Sticker_Web_dotnet.Repository
+{
+    public class CourierRepository : Repository<Courier>, ICourierRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CourierRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(Courier obj)
+        {
+            _db.Couriers.Update(obj);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repository/Interfaces/ICourierRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sticker_Web_dotnet.Models;
+
+namespace Sticker_Web_dotnet.Repository.Interfaces
+{
+    public interface ICourierRepository : IRepository<Courier>
+    {
+        Task UpdateAsync(Courier obj);
+    }
+}
