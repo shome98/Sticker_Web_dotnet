b9cee2d3af0d05877f7b3c2554a5bc4cd972c492
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 22:53:50 +0530

    order header repository add

---
+++ Repository/Interfaces/IOrderHeaderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Sticker_Web_dotnet.Models;
+
+namespace Sticker_Web_dotnet.Repository.Interfaces
+{
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        Task UpdateAsync(OrderHeader obj);
+        Task UpdateStatusAsync(int id, string orderStatus, string? paymentStatus = null);
+        Task UpdateStripePaymentIdAsync(int id, string sessionId, string paymentIntentId);
+    }
+}
---
+++ Repository/OrderHeaderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sticker_Web_dotnet.Models;
+using Sticker_Web_dotnet.Data;
+using Sticker_Web_dotnet.Repository.Interfaces;
+namespace Sticker_Web_dotnet.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+            await _db.SaveChangesAsync();
+        }
+        public async Task UpdateStatusAsync(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = await _db.OrderHeaders.FirstOrDefaultAsync(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+            await _db.SaveChangesAsync();
+        }
+        public async Task UpdateStripePaymentIdAsync(int id, string sessionId, string paymentIntentId)
+        {
+            var orderFromDb = await _db.OrderHeaders.FirstOrDefaultAsync(u => u.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
+            await _db.SaveChangesAsync();
+        }
+    }
+}
