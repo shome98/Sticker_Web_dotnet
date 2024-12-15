ce7624aa2e3002a66ed23044c791edf0bcd1f7c7
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 23:05:45 +0530

    connection string add

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection1")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
