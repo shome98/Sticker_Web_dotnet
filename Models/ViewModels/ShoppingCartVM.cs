ad1c2ede3465d6c96c534f0d8780dc132851c9de
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 20:32:31 +0530

    shopping cart and shopping cart view model add

---
+++ Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,11 @@
+﻿using Sticker_Web_dotnet.Models;
+
+namespace Sticker_Web_dotnet.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
