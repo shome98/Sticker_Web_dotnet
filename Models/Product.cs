952c5cccf1a0435f4ed56fed5b08581f3e46a823
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 20:32:12 +0530

    product and product view model add

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sticker_Web_dotnet.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        [ValidateNever]
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        [ValidateNever]
+        [Required]
+
+        public string? ImageUrl { get; set; }
+    }
+}
