4f2ab9f5b805db009af13e766e8734ffe10d51b7
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 22:53:14 +0530

    vendor repository add

---
+++ Repository/Interfaces/IVendorRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sticker_Web_dotnet.Models;
+
+namespace Sticker_Web_dotnet.Repository.Interfaces
+{
+    public interface IVendorRepository : IRepository<Vendor>
+    {
+        Task UpdateAsync(Vendor obj);
+    }
+}
---
+++ Repository/VendorRepository.cs
@@ -0,0 +1,21 @@
+﻿using Sticker_Web_dotnet.Models;
+using Sticker_Web_dotnet.Data;
+using Sticker_Web_dotnet.Repository.Interfaces;
+
+namespace Sticker_Web_dotnet.Repository
+{
+    public class VendorRepository : Repository<Vendor>, IVendorRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VendorRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(Vendor obj)
+        {
+            _db.Vendors.Update(obj);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
