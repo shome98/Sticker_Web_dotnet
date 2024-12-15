a1f1af558c0851b0839236afbcb68fd0c2482553
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 23:00:08 +0530

    product service add

---
+++ Services/Interfaces/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Sticker_Web_dotnet.Models;
+using Sticker_Web_dotnet.Models.ViewModels;
+
+namespace Sticker_web.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task AddAsync(Product obj);
+        Task<IEnumerable<Product>> GetAllAsync(string include);
+        Task<Product> GetAsync(int? id);
+        ProductVM ImageHandle(ProductVM productVM, IFormFile? file);
+        Task RemoveAsync(Product obj);
+        Task UpdateAsync(Product obj);
+    }
+}
