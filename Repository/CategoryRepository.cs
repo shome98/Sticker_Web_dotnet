672462bdbb2c2567f672e919a1ed12da959df4aa
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 22:51:56 +0530

    category repository add

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Sticker_Web_dotnet.Models;
+using Sticker_Web_dotnet.Data;
+using Sticker_Web_dotnet.Repository.Interfaces;
+
+namespace Sticker_Web_dotnet.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(Category obj)
+        {
+            _db.Categories.Update(obj);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repository/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sticker_Web_dotnet.Models;
+
+namespace Sticker_Web_dotnet.Repository.Interfaces
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Task UpdateAsync(Category obj);
+    }
+}
