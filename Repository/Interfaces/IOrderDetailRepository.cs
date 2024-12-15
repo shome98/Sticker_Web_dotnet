f44dd540612c72de8d047c5906fab83c7f6a01c4
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 22:54:03 +0530

    order detail repository add

---
+++ Repository/Interfaces/IOrderDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sticker_Web_dotnet.Models;
+
+namespace Sticker_Web_dotnet.Repository.Interfaces
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetail>
+    {
+        Task UpdateAsync(OrderDetail obj);
+    }
+}
---
+++ Repository/OrderDetailRepository.cs
@@ -0,0 +1,21 @@
+﻿using Sticker_Web_dotnet.Models;
+using Sticker_Web_dotnet.Data;
+using Sticker_Web_dotnet.Repository.Interfaces;
+
+namespace Sticker_Web_dotnet.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
