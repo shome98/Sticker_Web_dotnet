58a1d25a342401d23db5d33039bd68042a70a84f
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 20:34:58 +0530

    order view model add

---
+++ Models/ViewModels/OrderVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Sticker_Web_dotnet.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
