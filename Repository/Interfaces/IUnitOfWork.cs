d6bc3abc1b05bf3f13d2dcf17a8e4e21ce6872b6
Sowparna Shome <soupornoshome@gmail.com>
2024-12-15 22:54:17 +0530

    unit of work pattern add

---
+++ Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace Sticker_Web_dotnet.Repository.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+        IVendorRepository Vendor { get; }
+        ICourierRepository Courier { get; }
+        IShoppingCartRepository ShoppingCart { get; }
+        IApplicationUserRepository ApplicationUser { get; }
+        IOrderHeaderRepository OrderHeader { get; }
+        IOrderDetailRepository OrderDetail { get; }
+        Task SaveAsync();
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Sticker_Web_dotnet.Data;
+using Sticker_Web_dotnet.Repository.Interfaces;
+
+namespace Sticker_Web_dotnet.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
+        public IVendorRepository Vendor { get; private set; }
+        public ICourierRepository Courier { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
+            Vendor = new VendorRepository(_db);
+            Courier = new CourierRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
