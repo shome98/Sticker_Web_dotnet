88b2f77f133c815b0a1713440c140c4884cbf06d
Sowparna Shome <soupornoshome@gmail.com>
2024-12-16 20:02:46 +0530

    static details add

---
+++ Utility/SD.cs
@@ -0,0 +1,25 @@
+﻿namespace Sticker_Web_dotnet.Utility
+{
+    public static class SD
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Vendor = "Vendor";
+        public const string Role_Admin = "Admin";
+        public const string Role_Courier = "Courier";
+
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
+        public const string SessionCart = "SessionShoppingCart";
+
+    }
+}
