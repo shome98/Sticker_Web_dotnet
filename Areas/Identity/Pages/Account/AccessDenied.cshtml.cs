bb34dcfd8762df32a499f73d441b1dbf06a0dc95
Sowparna Shome <soupornoshome@gmail.com>
2024-12-16 19:57:43 +0530

    access denied page

---
+++ Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sticker_Web_dotnet.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
