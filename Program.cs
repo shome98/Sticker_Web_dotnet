e8bba184a10a202507241175334a2a82031d7635
Sowparna Shome <soupornoshome@gmail.com>
2024-12-17 23:53:31 +0530

    area add

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
