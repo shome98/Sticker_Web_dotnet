f73691021bd808db0d0cdb2390cf16a472d7f715
Sowparna Shome <soupornoshome@gmail.com>
2024-12-17 23:31:00 +0530

    stripe settings add

---
+++ Utility/StripeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Sticker_Web_dotnet.Utility
+{
+    public class StripeSettings
+    {
+        public string SecretKey { get; set; }
+        public string PublishableKey { get; set; }
+    }
+}
